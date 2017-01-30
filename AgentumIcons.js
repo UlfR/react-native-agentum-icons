a9f1f0093801bee9426c830d4a9017e2615e55fe
Gleb Stiblo <errandir@gmail.com>
2017-01-30 19:40:54 +0600

    update_font

--- AgentumIcons.js
+++ AgentumIcons.js
@@ -6,7 +6,7 @@
 import createIconSet from './lib/create-icon-set';
 import glyphMap from './glyphmaps/AgentumIcons.json';
 
-const iconSet = createIconSet(glyphMap, 'agentum', 'AgentumIcons.ttf');
+const iconSet = createIconSet(glyphMap, 'agentum', 'agentum.ttf');
 
 export default iconSet;
 
